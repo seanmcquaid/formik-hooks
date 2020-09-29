8266c606d2c08156422982eb8b12fcafcbe4c6c6
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-28 20:03:06 -0400

    finished use formik example, very cool

---
+++ src/components/Button.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = React.memo(styled.button``);
+
+export default Button;
--- src/containers/UseFormik.js
+++ src/containers/UseFormik.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { useFormik } from 'formik';
+import Form from '../components/Form';
+import Label from '../components/Label';
+import Input from '../components/Input';
+import Button from '../components/Button';
 
 const UseFormik = () => {
   const formik = useFormik({
-    initialValues: {},
+    initialValues: {
+      name: '',
+    },
     onSubmit: (values) => {
       console.log(values);
     },
   });
 
-  return ();
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <Label>Name</Label>
+      <Input
+        name='name'
+        type='text'
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      <Button type='submit'>Submit</Button>
+    </Form>
+  );
 };
 
 export default UseFormik;
